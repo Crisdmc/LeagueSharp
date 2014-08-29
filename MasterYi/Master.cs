122463dfa4311047c4947a63e130be479ec2e057
crisdmc <cristian.gohl@live.com>
2014-08-29 03:12:11 -0300

    Starting

---
+++ MasterYi/Master.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+using SharpDX;
+
+namespace LazyYi
+{
+    class Master
+    {
+        public static Obj_AI_Hero Player = ObjectManager.Player;
+
+        public static Spellbook sBook = Player.Spellbook;
+
+        public static Orbwalking.Orbwalker orbwalker;
+
+        public static SpellDataInst Qdata = sBook.GetSpell(SpellSlot.Q);
+        public static SpellDataInst Wdata = sBook.GetSpell(SpellSlot.W);
+        public static SpellDataInst Edata = sBook.GetSpell(SpellSlot.E);
+        public static SpellDataInst Rdata = sBook.GetSpell(SpellSlot.R);
+        public static Spell Q = new Spell(SpellSlot.Q, 600);
+        public static Spell W = new Spell(SpellSlot.W, 0);
+        public static Spell E = new Spell(SpellSlot.E, 0);
+        public static Spell R = new Spell(SpellSlot.R, 0);
+
+        public static void doLaneClear(Obj_AI_Base minion)
+        {
+            if (!minion.IsValidTarget())
+            {
+                return;
+            }
+
+            // Se o Q está configurado para usar
+            if (Script.Config.Item("useQLC").GetValue<bool>())
+            {
+                Q.Cast(minion);
+            }
+        }
+
+        public static void doCombo(Obj_AI_Hero target)
+        {
+            // verifica se target é válido
+            if (!target.IsValidTarget())
+            {
+                return;
+            }
+
+            // Se o Q está configurado para usar
+            if (Script.Config.Item("useQ").GetValue<bool>())
+            {
+                useQ(target);
+            }
+
+            // Se o E está configurado para usar
+            if (Script.Config.Item("useE").GetValue<bool>())
+            {
+                useE(target);
+            }
+
+            // Se o W está configurado para usar
+            if (Script.Config.Item("useW").GetValue<bool>())
+            {
+                useW(target);
+            }
+
+            // Se o R está configurado para usar
+            if (Script.Config.Item("useR").GetValue<bool>())
+            {
+                useR(target);
+            }
+        }
+
+        public static void useQ(Obj_AI_Hero target)
+        {
+            // Se o que não pode ser usado
+            if (!Q.IsReady())
+            {
+                return;
+            }
+
+            // implementar outras verificações, para dodge e tal.
+            Q.Cast(target);
+        }
+
+        public static void useR(Obj_AI_Hero target)
+        {
+            // Se o R não está disponível
+            if (!R.IsReady())
+            {
+                return;
+            }
+
+            float trueAARange = Player.AttackRange + target.BoundingRadius;
+
+            // Distancia até o target
+            float dist = Player.Distance(target);
+
+            if (dist < trueAARange)
+            {
+                R.Cast();
+            }
+        }
+
+        public static void useE(Obj_AI_Hero target)
+        {
+            // Se o E não está disponível
+            if (!E.IsReady())
+            {
+                return;
+            }
+
+            float trueAARange = Player.AttackRange + target.BoundingRadius;
+
+            // Distancia até o target
+            float dist = Player.Distance(target);
+            
+            if (dist < trueAARange) 
+            {
+                E.Cast();
+            }
+
+        }
+
+        public static void useW(Obj_AI_Hero target)
+        {
+            // Se o W não está disponível
+            if (!W.IsReady())
+            {
+                return;
+            }
+
+            // Verifica se HP está abaixo do configurado para usar o W
+            if (myHPPercent() < Script.Config.Item("useWon").GetValue<Slider>().Value)
+            {
+                W.Cast();
+            }
+        }
+
+        public static int myHPPercent()
+        {
+            return (int)((Player.Health / Player.MaxHealth) * 100);
+        }
+    }
+}
---
+++ MasterYi/Script.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+
+
+namespace LazyYi
+{
+    internal class Script
+    {
+
+        public const string CharName = "MasterYi";
+
+        public static Menu Config;
+
+        public static Obj_AI_Hero target;
+
+        public Script()
+        {
+            /* CallBAcks */
+            CustomEvents.Game.OnGameLoad += onLoad;
+
+        }
+
+        private static void onLoad(EventArgs args)
+        {
+
+            Game.PrintChat("LazyYi - by Crisdmc");
+
+            try
+            {
+                Config = new Menu("LazyYi", "MasterYi", true);
+                // OrbWalker
+                Config.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+                Master.orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker"));
+                
+                // Target Selector
+                var TargetSelectorMenu = new Menu("Target Selector", "Target Selector");
+                SimpleTs.AddToMenu(TargetSelectorMenu);
+                Config.AddSubMenu(TargetSelectorMenu);
+                
+                // Combo
+                Config.AddSubMenu(new Menu("Combo", "combo"));
+                Config.SubMenu("combo").AddItem(new MenuItem("useQ", "Use Q")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("useW", "Use W")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("useWon", "Use W on %")).SetValue(new Slider(35, 100, 0));
+                Config.SubMenu("combo").AddItem(new MenuItem("useE", "Use E")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("useR", "Use R")).SetValue(true);
+
+                // Lane Clear
+                Config.AddSubMenu(new Menu("Lane Clear", "laneclear"));
+                Config.SubMenu("laneclear").AddItem(new MenuItem("useQLC", "Use Q")).SetValue(true);
+
+                Config.AddToMainMenu();
+                Drawing.OnDraw += onDraw;
+                Game.OnGameUpdate += OnGameUpdate;
+
+                GameObject.OnCreate += OnCreateObject;
+                GameObject.OnDelete += OnDeleteObject;
+            }
+            catch
+            {
+                Game.PrintChat("MasterYi script error!");
+            }
+
+        }
+
+        private static void OnGameUpdate(EventArgs args)
+        {
+
+            if (Master.orbwalker.ActiveMode.ToString() == "Combo")
+            {
+                // Se tem alguém no range do Q, pega target
+                if(Master.Q.IsReady())
+                {
+                    target = SimpleTs.GetTarget(Master.Q.Range, SimpleTs.DamageType.Physical);
+                }
+                    
+                // Pega target pelo range básico
+                else
+                {
+                    target = SimpleTs.GetTarget(250, SimpleTs.DamageType.Physical); //125
+                }
+                    
+
+                Master.doCombo(target);
+            }
+
+            if (Master.orbwalker.ActiveMode.ToString() == "Mixed")
+            {
+                
+            }
+
+            if (Master.orbwalker.ActiveMode.ToString() == "LaneClear")
+            {
+                // Verifica se o Q está pronto
+                if (Master.Q.IsReady())
+                {
+                    // Obtem todos os minions do time inimigo, no range do Q
+                    var allMinions = MinionManager.GetMinions(ObjectManager.Player.ServerPosition, Master.Q.Range, MinionTypes.All, MinionTeam.Enemy);
+                    var closestMinion = new Obj_AI_Base();
+
+                    foreach (Obj_AI_Base minion in allMinions)
+                    {
+                        if (Master.Player.Distance(minion.Position) < Master.Player.Distance(closestMinion.Position))
+                        {
+                            closestMinion = minion;
+                        }
+                    }
+
+                    Master.doLaneClear(closestMinion);
+                }
+            }
+        }
+
+        private static void onDraw(EventArgs args)
+        {
+            Drawing.DrawCircle(Master.Player.Position, Master.Q.Range, Color.Blue);
+        }
+
+        private static void OnCreateObject(GameObject sender, EventArgs args)
+        {
+        }
+
+        private static void OnDeleteObject(GameObject sender, EventArgs args)
+        {
+        }
+    }
+}
