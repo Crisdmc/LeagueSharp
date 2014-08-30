f99947b5802704eaa8225f78fd32beab40fe16a5
crisdmc <cristian.gohl@live.com>
2014-08-30 07:14:28 -0300

    Beginning to develop the jungle slack.

---
+++ MasterYi/entity/JungleCamp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using MasterYi.entity;
+
+namespace MasterYi.entity
+{
+    class JungleCamp
+    {
+        public Vector3 position { get; set; }
+        public TimeSpan spawnTime { get; set; }
+        public TimeSpan respawnTime { get; set; }
+        public List<JungleMinion> minions { get; set; }
+    }
+}
---
+++ MasterYi/entity/JungleMinion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+
+namespace MasterYi.entity
+{
+    class JungleMinion
+    {
+        public String name { get; set; }
+        public bool isDead { get; set; }
+        public GameObject minion { get; set; }
+
+
+        public JungleMinion(String name)
+        {
+            this.name = name;
+        }
+    }
+}
