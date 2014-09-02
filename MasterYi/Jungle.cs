99de2b9f9e65b6188477154159a70c4ed7e28940
crisdmc <cristian.gohl@live.com>
2014-09-02 03:46:24 -0300

    Removed console write commands.

--- MasterYi/Jungle.cs
+++ MasterYi/Jungle.cs
@@ -46,8 +46,8 @@ namespace MasterYi
         {
             if (camp.Team == GameObjectTeam.Neutral)
             {
-                Console.WriteLine("Create");
-                Console.WriteLine(camp.Name);
+                //Console.WriteLine("Create");
+                //Console.WriteLine(camp.Name);
             }
         }
 
@@ -55,8 +55,8 @@ namespace MasterYi
         {
             if (camp.Team == GameObjectTeam.Neutral && camp.IsDead)
             {
-                Console.WriteLine("Delete");
-                Console.WriteLine(camp.Name);
+                //Console.WriteLine("Delete");
+                //Console.WriteLine(camp.Name);
                 jungleMinionDied(camp.Name);
             }
         }
@@ -73,8 +73,8 @@ namespace MasterYi
 
             _player.IssueOrder(GameObjectOrder.MoveTo, blueCamp.position);
 
-            Console.WriteLine("Meu Time");
-            Console.WriteLine(_player.Team);
+            //Console.WriteLine("Meu Time");
+            //Console.WriteLine(_player.Team);
             
             var testando = ObjectManager.Get<Obj_AI_Minion>().Where(minion => minion.Team == GameObjectTeam.Neutral);
             List<Obj_AI_Minion> minions = testando.ToList();
